2edcd53fc079f22ed01ff4b28bc63b1e3bc6177a
Иван Просвиров <ivan.prrrosvirov@gmail.com>
2026-06-10 14:13:06 +0300

    Print target platform when ask user to install docker for cross-compilation

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -1,11 +1,12 @@
+import { SpecificPlatform } from "../commands/build/builders.js";
 import exec from "./exec.js";
 
-export async function checkDocker() {
+export async function checkDocker(platform: SpecificPlatform) {
   try {
     await exec("docker info", { ignore: true });
   } catch {
     throw new Error(
-      "Docker is required to build Android targets.\n" +
+      `Docker is required to build ${platform} target.\n` +
         "Please install Docker: https://docs.docker.com/get-docker/",
     );
   }
