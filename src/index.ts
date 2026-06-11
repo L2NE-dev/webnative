3c6ab28a3e194d3408ca7cd7ece6fed13d15ddcb
Иван Просвиров <ivan.prrrosvirov@gmail.com>
2026-06-11 05:21:23 +0300

    Add top level exception handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { program } from "commander";
 import { registerBuild } from "./commands/build/build.js";
 import { registerInit } from "./commands/init.js";
+import { processUncaughtException } from "./utils/exception.js";
+
+process.on("uncaughtException", processUncaughtException);
+process.on("unhandledRejection", processUncaughtException);
 
 registerInit(program);
 registerBuild(program);
