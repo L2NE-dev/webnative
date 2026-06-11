d925ab2dd2a997394349dd4d370b6cb0a666d676
Иван Просвиров <ivan.prrrosvirov@gmail.com>
2026-06-11 05:23:28 +0300

    Docker on windows could be also not running at the moment, ask the user to start the docker daemon

--- src/utils/docker.ts
+++ src/utils/docker.ts
@@ -6,8 +6,9 @@ export async function checkDocker(platform: SpecificPlatform) {
     await exec("docker info", { ignore: true });
   } catch {
     throw new Error(
-      `Docker is required to build ${platform} target.\n` +
-        "Please install Docker: https://docs.docker.com/get-docker/",
+      `Docker is required to build ${platform} target and must be running.\n` +
+        "Please start Docker Desktop and try again.\n" +
+        "Install Docker if not installed: https://docs.docker.com/get-docker/",
     );
   }
 }
